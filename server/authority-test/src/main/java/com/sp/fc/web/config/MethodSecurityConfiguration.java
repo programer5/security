06b876023d4682c49b46db7d403970db4cacf8db
minseo23 <neverdie4757@gmail.com>
2021-09-28 00:11:10 +0900

    권한의 큰 그림

---
+++ server/authority-test/src/main/java/com/sp/fc/web/config/MethodSecurityConfiguration.java
@@ -0,0 +1,6 @@
+package com.sp.fc.web.config;
+
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+}
