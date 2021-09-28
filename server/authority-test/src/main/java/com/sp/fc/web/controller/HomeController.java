3dfdddc4760e84799206eee8f2606c92119302e5
minseo23 <neverdie4757@gmail.com>
2021-09-28 23:07:37 +0900

    권한

--- server/authority-test/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/authority-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.sp.fc.web.controller;
 
+import com.sp.fc.web.service.SecurityMessageService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    @GetMapping("/greeting")
-    public String greeting() {
-        return "hello";
+    private final SecurityMessageService securityMessageService;
+
+    public HomeController(SecurityMessageService securityMessageService) {
+        this.securityMessageService = securityMessageService;
+    }
+
+
+    @PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name) {
+        return "hello " + securityMessageService.message(name);
     }
 }
