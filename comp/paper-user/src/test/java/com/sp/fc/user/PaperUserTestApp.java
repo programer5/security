b335fb17f799d9016a7ca2cff34aa996dc020bbe
minseo23 <neverdie4757@gmail.com>
2021-10-03 15:12:08 +0900

    메인화면과 로그인화면

--- comp/paper-user/src/test/java/com/sp/fc/user/PaperUserTestApp.java
+++ comp/paper-user/src/test/java/com/sp/fc/user/PaperUserTestApp.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class PaperUserTestApp {
 
     public static void main(String[] args) {
