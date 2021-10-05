e07daeb960db37f693a08bded78375d6d7711e3d
minseo23 <neverdie4757@gmail.com>
2021-10-06 00:29:00 +0900

    AuthToken을 이용한 로그인

--- server/security-advanced/src/main/java/com/sp/fc/web/AdvancedSecurityApplication.java
+++ server/security-advanced/src/main/java/com/sp/fc/web/AdvancedSecurityApplication.java
@@ -3,7 +3,10 @@ package com.sp.fc.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.sp.fc.config",
+        "com.sp.fc.web"
+})
 public class AdvancedSecurityApplication {
 
     public static void main(String[] args) {
