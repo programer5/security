0f74bbdbae67241ea1522f723cd32d6d8a840d85
minseo23 <neverdie4757@gmail.com>
2021-10-07 00:29:08 +0900

    RefreshToken을 이용한 로그인

--- server/security-advanced/src/main/java/com/sp/fc/web/config/JWTCheckFilter.java
+++ server/security-advanced/src/main/java/com/sp/fc/web/config/JWTCheckFilter.java
@@ -1,5 +1,6 @@
 package com.sp.fc.web.config;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.sp.fc.user.domain.SpUser;
 import com.sp.fc.user.service.SpUserService;
 import org.springframework.http.HttpHeaders;
@@ -43,7 +44,7 @@ public class JWTCheckFilter extends BasicAuthenticationFilter {
             SecurityContextHolder.getContext().setAuthentication(userToken);
             chain.doFilter(request, response);
         } else {
-            throw new AuthenticationException("Token is not valid");
+            throw new TokenExpiredException("Token is not valid");
         }
     }
 }
--- server/security-advanced/src/main/java/com/sp/fc/web/config/JWTUtil.java
+++ server/security-advanced/src/main/java/com/sp/fc/web/config/JWTUtil.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 public class JWTUtil {
 
     private static final Algorithm ALGORITHM = Algorithm.HMAC256("minseo");
-    private static final long AUTH_TIME = 20*60;
+    private static final long AUTH_TIME = 2;
     private static final long REFRESH_TIME = 60*60*24;
 
     public static String makeAuthToken(SpUser user) {
